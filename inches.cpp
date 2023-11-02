46b2d0e92b3e52c3c02a0b9c23f6ff233afd6dd8
hussnainnaz <148995940+hussnainnaz@users.noreply.github.com>
2023-11-02 01:01:53 -0700

    Add files via upload

---
+++ inches.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float inch;
+	float cm;
+	cout<<"Enter inches:"<<endl;
+	cin>>inch;
+	cm=2.54*inch;
+	cout<<"value of cm:"<<cm;
+}
---
+++ two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A,B;
+	cout<<"Enter values of A,B:"<<endl;
+	cin>>A>>B;
+	if (A>B)
+	cout<<"A is larger";
+	else
+	cout<<"B is larger";
+	return 0;
+}
